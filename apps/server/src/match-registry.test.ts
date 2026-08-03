be6e495f0da98a76ea02491f28db625264b9d1be
Patrashu <109667960+9barcode@users.noreply.github.com>
2026-08-03 20:58:12 +0900

    회귀 테스트의 플레이어 튜플 타입 수정

--- apps/server/src/match-registry.test.ts
+++ apps/server/src/match-registry.test.ts
@@ -9,7 +9,7 @@ const players = [
 describe("MatchRegistry 메모리 정리", () => {
   it("종료된 경기 객체와 해당 플레이어 연결을 함께 제거한다", () => {
     const registry = new MatchRegistry();
-    registry.create("old-match", [...players]);
+    registry.create("old-match", [players[0], players[1]]);
 
     expect(registry.remove("old-match")).toBe(true);
     expect(registry.getCurrentForPlayer("player-1")).toBeNull();
@@ -19,8 +19,8 @@ describe("MatchRegistry 메모리 정리", () => {
 
   it("같은 플레이어가 새 경기에 들어간 뒤에는 새 연결을 지우지 않는다", () => {
     const registry = new MatchRegistry();
-    registry.create("old-match", [...players]);
-    const newMatch = registry.create("new-match", [...players]);
+    registry.create("old-match", [players[0], players[1]]);
+    const newMatch = registry.create("new-match", [players[0], players[1]]);
 
     registry.remove("old-match");
 
