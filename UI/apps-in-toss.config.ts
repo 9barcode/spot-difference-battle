8b1491ff40725706890200207ea89b6dcca4d54c
ssbluegana <ssbluegana@gmail.com>
2026-08-31 22:16:25 +0900

    chore: add Apps in Toss SDK 3 config

---
+++ UI/apps-in-toss.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "@apps-in-toss/web-framework/config";
+
+export default defineConfig({
+  appName: "spot-difference-syk",
+  brand: {
+    primaryColor: "#7C3AED",
+  },
+  webView: {},
+  permissions: [],
+  webBundleDir: "dist",
+});
